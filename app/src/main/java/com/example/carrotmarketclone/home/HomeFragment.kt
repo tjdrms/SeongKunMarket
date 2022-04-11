94655b8f0c8d1df55d4a0064d642bdf9fd6fe26b
tjdrms <tjdrms0116>
2022-04-11 12:00:34 +0900

    중고 거래 어플리케이션(패스트캠퍼스 실습용)

--- app/src/main/java/com/example/carrotmarketclone/home/HomeFragment.kt
+++ app/src/main/java/com/example/carrotmarketclone/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.carrotmarketclone.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.carrotmarketclone.DBKey.Companion.DB_ARTICLES
 import com.example.carrotmarketclone.R
 import com.example.carrotmarketclone.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ChildEventListener
@@ -59,6 +61,17 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
 
         fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
         fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+        fragmentHomeBinding.addFloatingButton.setOnClickListener {
+            context?.let {
+                if(auth.currentUser != null) {
+                    val intent = Intent(requireContext(), AddArticleActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Snackbar.make(view, "로그인 후 사용해주세요.", Snackbar.LENGTH_LONG).show()
+                }
+            }
+        }
 
         articleDB.addChildEventListener(listener)
     }
